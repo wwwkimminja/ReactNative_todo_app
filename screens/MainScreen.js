e316ba48a11bab87b9f8baad650f74b5765dafc7
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-06-17 14:14:10 +0900

    style

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,23 +1,47 @@
 import React from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Platform, StyleSheet, Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Form from '../components/Form'
 import TodoItem from '../components/TodoItem'
+import { useSelector } from 'react-redux'
 
 
 const MainScreen = () => {
+  const todos = useSelector(state => state.todo.todos);
+  const todoTasks = todos.filter((item) => item.state === 'todo')
+  const completedTasks = todos.filter((item) => item.state === 'done')
+
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'default'} />
       <Text style={styles.pageTitle}>Todo App</Text>
       <View style={styles.listView}>
-        <Text style={styles.listTitle}>할 일  </Text>
-        <TodoItem />
+        <Text style={styles.listTitle}>to do</Text>
+        {todoTasks.length !== 0 ? (
+          <FlatList
+            data={todoTasks}
+            renderItem={({ item }) => <TodoItem {...item}
+              keyExtractor={(item) => item.id}
+            />} />
+        ) : (
+          <Text style={styles.emptyListText}>nothing todo</Text>
+        )}
       </View>
       <View style={styles.separator} />
       <View style={styles.listView}>
-        <Text style={styles.listTitle}>완료된 일 </Text>
+        <Text style={styles.listTitle}>done </Text>
+        {completedTasks.length !== 0 ? (
+          <FlatList
+            data={completedTasks}
+            renderItem={({ item }) => <TodoItem {...item}
+              keyExtractor={(item) => item.id}
+            />}
+          />
+        ) : (
+          <Text style={styles.emptyListText}>nothing be done</Text>
+        )}
       </View>
       <Form />
 
@@ -57,4 +81,12 @@ const styles = StyleSheet.create({
     fontSize: 41,
     fontWeight: '500'
   },
+  emptyListText: {
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    fontSize: 15,
+    lineHeight: 20,
+    color: "#737373"
+  }
 })
